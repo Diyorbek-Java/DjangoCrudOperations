2093e8ab5ee39af5b13746162d4650f5622e157a
Diyorbek-Java <diorbeckturdiev@gmail.com>
2024-07-04 17:40:46 +0500

    individual record procesing

--- website/views.py
+++ website/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth import authenticate,login,logout
 from django.contrib import messages
+from .forms import SignUpForm
+from .models import Record
 
 def home(request):
+    records = Record.objects.all()
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
@@ -15,7 +18,7 @@ def home(request):
             messages.success(request,"Ther is an error")
             return redirect('home')
     else:
-        return render(request,'home.html',{})
+        return render(request,'home.html',{'records':records})
 
 def logout_user(request):
     logout(request)
@@ -23,4 +26,26 @@ def logout_user(request):
     return redirect('home')
 
 def register_user(request):
-    return render(request,'register.html',{})
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username,password=password)
+            login(request,user)
+            messages.success(request,"You succesfully registered")
+            return redirect('home')
+        else:
+            return render(request, 'register.html', {'form': form})
+    else:
+        form = SignUpForm()
+        return render(request,'register.html',{'form':form})
+    
+def custom_record(request,pk):
+    if request.user.is_authenticated:
+        custom_record = Record.objects.get(id=pk)
+        return render(request,'record.html',{'custom_record':custom_record})
+    else:
+        messages.success(request,"You must be logged in")
+        return redirect('home')
