25394d7a8a97659b92a444bd73d0cd2cfad0885d
Diyorbek-Java <diorbeckturdiev@gmail.com>
2024-07-04 10:41:03 +0500

    user registration

--- website/views.py
+++ website/views.py
@@ -1,9 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.contrib.auth import authenticate,login,logout
 from django.contrib import messages
 
 def home(request):
-    return render(request,'home.html',{})
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request,user)
+            messages.success(request,"you have logged in")
+            return redirect('home')
+        else:
+            messages.success(request,"Ther is an error")
+            return redirect('home')
+    else:
+        return render(request,'home.html',{})
 
 def logout_user(request):
-    pass
+    logout(request)
+    messages.success(request,"You are logged out")
+    return redirect('home')
+
+def register_user(request):
+    return render(request,'register.html',{})
